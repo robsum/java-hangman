eed162f0e04bdcf139bcf5705da56abaabe0f795
Robert Sumara <sumara.rob@gmail.com>
2022-02-06 13:26:57 +0100

    Generating random word

--- src/main/java/pl/edu/agh/hangman/Hangman.java
+++ src/main/java/pl/edu/agh/hangman/Hangman.java
@@ -1,6 +1,13 @@
 package pl.edu.agh.hangman;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Hangman {
+
+    public static int WORD_TO_FIND_INDEX = 0;
 
     public static final String[] HANGMANPICS = new String[]{
             "  +---+\n" +
@@ -54,6 +61,15 @@ public class Hangman {
                     "========"
     };
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // Hangman Pics: 0 - 6
+        System.out.println(HANGMANPICS[0]);
+
+        Words words = new Words();
+        String wordToGuess = words.getRandomWord();
+
+        System.out.println(wordToGuess);
     }
+
+
 }
---
+++ src/main/java/pl/edu/agh/hangman/Words.java
@@ -0,0 +1,43 @@
+package pl.edu.agh.hangman;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Words {
+
+    public final String filepath = "src/main/resources/slowa.txt";
+
+    private ArrayList<String> words;
+
+    public Words() throws IOException {
+        // read words file:
+        words = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader(filepath));
+        try {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+                words.add(line);
+            }
+            String everything = sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            br.close();
+        }
+    }
+    public static int getRandomNumber(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
+    }
+
+    public String getRandomWord() {
+        int index = getRandomNumber(0, words.size());
+        return words.get(index);
+    }
+}
