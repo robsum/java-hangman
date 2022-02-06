a1c0425dd450c1e8e04fa558f590c309e6089960
Robert Sumara <sumara.rob@gmail.com>
2022-02-06 14:04:51 +0100

    character getter class

---
+++ src/main/java/pl/edu/agh/hangman/CharacterGetter.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.hangman;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class CharacterGetter {
+    public String getCharacterFromUser() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("guess> ");
+        char ch = scanner.next().toUpperCase(Locale.ROOT).charAt(0);
+        return String.valueOf(ch);
+    }
+}
--- src/main/java/pl/edu/agh/hangman/Hangman.java
+++ src/main/java/pl/edu/agh/hangman/Hangman.java
@@ -4,10 +4,9 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class Hangman {
-
-    public static int WORD_TO_FIND_INDEX = 0;
 
     public static final String[] HANGMANPICS = new String[]{
             "  +---+\n" +
@@ -63,12 +62,40 @@ public class Hangman {
 
     public static void main(String[] args) throws IOException {
         // Hangman Pics: 0 - 6
-        System.out.println(HANGMANPICS[0]);
+        int wrongAnswers = 0;
+
+        System.out.println(HANGMANPICS[wrongAnswers]);
 
         Words words = new Words();
-        String wordToGuess = words.getRandomWord();
+        String wordToGuess = words.getRandomWord().toUpperCase(Locale.ROOT);
 
         System.out.println(wordToGuess);
+
+        CharacterGetter characterGetter = new CharacterGetter();
+
+        String ch = characterGetter.getCharacterFromUser();
+        while (!ch.equals("Q") && wrongAnswers < 6) {
+            if (CharacterGuesser.isCharacterInString(ch, wordToGuess)) {
+                // print word with guessed chars
+                System.out.println(wordToGuess);
+
+            } else {
+                wrongAnswers++;
+                System.out.println(HANGMANPICS[wrongAnswers]);
+            }
+            if (wrongAnswers == 6) {
+                break;
+            }
+            ch = characterGetter.getCharacterFromUser();
+        }
+
+        if (wrongAnswers == 6) {
+            System.out.println("Przegrałeś!");
+        } else {
+            System.out.println("Wygrałeś!");
+        }
+
+
     }
 
 
